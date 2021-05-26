acc1cbb3a91269d5bedc84f12fac6183d293d06d
Will Cooke <wrcooke@wrcooke.net>
2021-05-26 04:44:25 -0500

    Got calculations and drawings working

--- c_progs/atten.c
+++ c_progs/atten.c
@@ -74,6 +74,7 @@
 
 typedef struct atten
 {
+  double db;
   double r0;
   double r1;
   double r2;
@@ -105,42 +106,111 @@ int main(int argc, char** argv)
   r = dbToRatio(60);
   printf("60 db is %f \n", r);
 
+  double db = 1;
+  while(db < 70)
+    {
+      attenuator_t atn;
+      atn = calcPi(50, db);
+      drawPi(atn);
+      atn = calcT(50,db);
+      drawT(atn);
+      atn = calcBridgedT(50,db);
+      drawBridgedT(atn);
+      db *= 2;
+    };
 
 
   return rtn;
 }
 
+// Values go into R3,R4
 attenuator_t calcPi(double r0, double db)
 {
   attenuator_t rtn;
+  double N = dbToRatio(db);
+  rtn.db = db;
+  rtn.r0 = r0;
+  rtn.r3 = r0* (N+1)/(N-1);
+  rtn.r4 = r0* (N*N-1)/(2*N);
 
   return rtn;
 }
 
+// Values go into R1,R2
 attenuator_t calcT(double r0, double db)
 {
   attenuator_t rtn;
+  double N = dbToRatio(db);
+  rtn.db = db;
+  rtn.r0 = r0;
+  rtn.r1 = r0 * (N-1)/(N+1);
+  rtn.r2 = r0 * (2*N)/(N*N-1);
 
   return rtn;
 }
 
+// Values into R5,R6
 attenuator_t calcBridgedT(double r0, double db)
 {
   attenuator_t rtn;
+  double N = dbToRatio(db);
+  rtn.db = db;
+  rtn.r0 = r0;
+  rtn.r5 = r0*(N-1);
+  rtn.r6 = r0*(1.0/(N+1.0));
 
   return rtn;
 }
 
 void drawPi(attenuator_t a)
 {
+  printf("\n Pi attenuator for %3.2f db and %3.1f Ohms \n", a.db, a.r0);
+  printf("        %3.2f   \n", a.r4);
+  printf("------------VVV------------\n");
+  printf("       |           |       \n");
+  printf("       Z           Z       \n");
+  printf("  %3.2fZ           Z %3.2f \n", a.r3);
+  printf("       Z           Z       \n");
+  printf("       |           |       \n");
+  printf("---------------------------\n");
+  printf("                           \n");
+  
 }
 
 void drawT(attenuator_t a)
 {
+  printf("\n  T attenuator for %3.2f db and %3.1f Ohms \n", a.db, a.r0);
+  printf("      %3.2f                 ",a.r1);
+  printf("-------VVV-------VVV-------\n");
+  printf("             |             \n");
+  printf("             Z             \n");
+  printf("             Z %3.2f       \n", a.r2);
+  printf("             Z             \n");
+  printf("             |             \n");
+  printf("---------------------------\n");
+  printf("                           \n");
+  
 }
 
 void drawBridgedT(attenuator_t a)
 {
+  printf("\n Bridged T attenuator for %3.2f db and %3.1f Ohms \n", a.db, a.r0);
+  printf("                           \n");
+  printf("            VVV            \n");
+  printf("-------VVV-------VVV-------\n");
+  printf("    |                 |    \n");
+  printf("    Z                 Z    \n");
+  printf("    Z %3.2f           Z  %3.2f \n", a.r0);
+  printf("    Z                 Z    \n");
+  printf("    |                 |    \n");
+  printf("    -------------------    \n");
+  printf("             |             \n");
+  printf("             Z             \n");
+  printf("             Z %3.2f       \n", a.r6);
+  printf("             Z             \n");
+  printf("             |             \n");
+  printf("---------------------------\n");
+  printf("                           \n");
 }
 
 double dbToRatio(double db)
